266e25fb4a853d6622c1489d88ac2fbb8ba75177
Murat <62623322+MuratOldman@users.noreply.github.com>
2020-05-08 15:32:28 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,6 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("try to do best");
+         System.out.println("try to do best");
     }
 }
