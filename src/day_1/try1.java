88c444edc8a61b7f41ce014b9b1320a06c6b36d5
MuratOldman <muratkara15102@gmail.com>
2020-05-08 15:27:33 -0400

    try2.java

---
+++ src/day_1/try1.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class try1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day_1/try2.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class try2 {
+    public static void main(String[] args) {
+
+    }
+}
